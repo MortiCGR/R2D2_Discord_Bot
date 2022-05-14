f5fb69bca947ceb8ba2fcc74795a52c3676ae68e
Mortimerr <cgrivas48@gmail.com>
2022-05-14 16:04:12 +0200

    add emoji

--- welcomeMessage.ts
+++ welcomeMessage.ts
@@ -5,23 +5,20 @@ export default (client : Client) => {
     // const channelId = `963913950620176386`; // RB welcome channel
 
     client.on('guildMemberAdd', (member) => {
-        const message = `Hi! <@${member.id}> Welcome to RedBridge! Just a quick summary of our guilds :
- 
-        <:RB1:974826460084863047> Redbridge - 30k waves per season - 50k minimum career waves
-        :RB2: Redbridge 2 - 20k waves per season - 10k minimum career waves
-        :RB3: Redbridge 3 - 8k waves per season - 5k minimum career waves 
-        :EXT: EXT - 8k waves per season - 5k minimum career waves 
-        :rb6: Redbridge 6 - 1K waves per season - No minimum career wave to join
-        :rb7: Redbridge 7 - 1K waves per season - No minimum career wave to join
-        :EXT: EXT2 - 1 wave per season - No minimum career wave to join
-         
-        - We also have a guest channel if you're just stopping by to say hi
-         
-         If you want to join can you please provide us with : 
-        In-Game Name :
-        Waves : 
-        Estimated Waves/Season (5 day period) :
-        Thank you!`
+        let message = `Hi! <@${member.id}> Welcome to RedBridge! Just a quick summary of our guilds :\n\n`
+        message += `${member.guild.emojis.cache.find(emoji => emoji.name === 'RB1')} Redbridge - 30k waves per season - 50k minimum career waves\n`
+        message += `:RB2: Redbridge 2 - 20k waves per season - 10k minimum career waves\n`
+        message += `:RB3: Redbridge 3 - 8k waves per season - 5k minimum career waves\n`
+        message += `:EXT: EXT - 8k waves per season - 5k minimum career waves\n`
+        message += `:rb6: Redbridge 6 - 1K waves per season - No minimum career wave to join\n`
+        message += `:rb7: Redbridge 7 - 1K waves per season - No minimum career wave to join\n`
+        message += `:EXT: EXT2 - 1 wave per season - No minimum career wave to join\n\n`
+        message += `- We also have a guest channel if you're just stopping by to say hi\n\n`
+        message += `If you want to join can you please provide us with :\n`
+        message += `In-Game Name :\n`
+        message += `Waves :\n`
+        message += `Estimated Waves/Season (5 day period) :\n`
+        message += `Thank you!\n`
 
         const channel = member.guild.channels.cache.get(channelId)
         
