1274acbe8b6aed9cd91d5d66c1b76736a7189786
Mortimerr <cgrivas48@gmail.com>
2022-05-14 02:21:28 +0200

    add emoji rb1

--- index.ts
+++ index.ts
@@ -7,7 +7,8 @@ const client = new DiscordJS.Client({
     intents : [
         Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_MEMBERS,
-        Intents.FLAGS.GUILD_MESSAGES
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS
     ]
 })
 
