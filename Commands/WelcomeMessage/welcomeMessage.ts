04c5bf8ad8e877681e03997c67051b71ff5bbf61
Mortimerr <cgrivas48@gmail.com>
2023-05-27 12:24:44 +0200

    Update welcome message

--- Commands/WelcomeMessage/welcomeMessage.ts
+++ Commands/WelcomeMessage/welcomeMessage.ts
@@ -14,9 +14,10 @@ export default (client : Client) => {
         let message = `Hi! <@${member.id}> Welcome to RedBridge! Just a quick summary of our guilds :\n\n`
         message += `${member.guild.emojis.cache.find(emoji => emoji.name === 'RB1')} Redbridge - 50k waves per season - 50k minimum career waves\n`
         message += `${member.guild.emojis.cache.find(emoji => emoji.name === 'RB2')} Redbridge 2 - 30k waves per season - 10k minimum career waves\n`
+        message += `${member.guild.emojis.cache.find(emoji => emoji.name === 'GlanceReviver')} GlanceReviver - 10k waves per season - 5k minimum career waves\n`
         message += `${member.guild.emojis.cache.find(emoji => emoji.name === 'RB3')} Redbridge 3 - 8k waves per season - 5k minimum career waves\n`
-        message += `${member.guild.emojis.cache.find(emoji => emoji.name === 'EXT')} -EXT- - 5k waves per season - 5k minimum career waves\n`
-        message += `${member.guild.emojis.cache.find(emoji => emoji.name === 'rb6')} Redbridge 6 - 1K waves per season - No minimum career wave to join\n`
+        message += `${member.guild.emojis.cache.find(emoji => emoji.name === 'EXT')} -EXT- - 8k waves per season - 5k minimum career waves\n`
+        message += `${member.guild.emojis.cache.find(emoji => emoji.name === 'rb6')} Redbridge 6 - 3K waves per season - No minimum career wave to join\n`
         message += `${member.guild.emojis.cache.find(emoji => emoji.name === 'rb7')} Redbridge 7 - 1K waves per season - No minimum career wave to join\n`
         message += `${member.guild.emojis.cache.find(emoji => emoji.name === 'EXT')} -EXT2- - 1 wave per season - No minimum career wave to join\n\n`
         message += `- We also have a guest channel if you're just stopping by to say hi\n\n`
