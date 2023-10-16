6d08f9bdbf0f8c495ebe85a44c4bdcdaa357cf24
Mortimerr <cgrivas48@gmail.com>
2023-10-17 00:15:40 +0200

    remove colon from welcome message

--- Commands/WelcomeMessage/welcomeMessage.js
+++ Commands/WelcomeMessage/welcomeMessage.js
@@ -9,7 +9,7 @@ exports.default = (client) => {
         // {
         //     return
         // }
-        let message = `Hi! <@${member.id}> Welcome to RedBridge! ${member.guild.emojis.cache.find(emoji => emoji.name === 'RB_COMP_GUILD')} :\n\n`;
+        let message = `Hi! <@${member.id}> Welcome to RedBridge! ${member.guild.emojis.cache.find(emoji => emoji.name === 'RB_COMP_GUILD')}\n\n`;
         message += `For a list of our guilds and their seasonal wave requirements, please check the :pushpin: **pinned message** :pushpin:\n\n`;
         message += `We also have a guest channel if you're just stopping by to say hi (please still provide your in-game name and waves)\n\n`;
         message += `If you want to join can you please provide us with :\n`;
